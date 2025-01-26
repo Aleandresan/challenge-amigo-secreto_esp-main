8da78364e9f09eecbbe4f8ae6eaca681e588fa97
Sandoval <alejandro.sandoval@gilbarco.com>
2025-01-26 18:01:59 -0300

    agregar nombres amigos funcion

--- app.js
+++ app.js
@@ -1,2 +1,45 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-//
+
+let amigos []; // almacenar nombres
+
+// agregar nombres amigos funcion
+function agragarAmigo(){
+    const inputAmigo = document.getElementById("amigo");
+    const nombreAmigo = inputAmigo.ariaValueMax.trim();
+   
+    //valida que no este en blanco nommbre
+    if (nombreAmigo === "") {
+        alert ("ingresa un nombre");
+        return;
+    }
+
+    //Validar que nombre no este duplicado
+    if (amigos.includes(nombreAmigo)){
+        alert (`El nombre ${nombreAmigo} ya esta en lista`);
+        return;
+    }
+
+    // agregar nombre al array amigos
+    amigos.push(nombreAmigo);
+
+    //limpiar campo de entrada
+    inputAmigo.nodeValue = "";
+
+    //Actualizar lista en .HTML
+
+}
+
+
+
+
+
+// agrega a la lista
+
+
+
+
+
+// sorteo
+
+
+//entrega nombre
